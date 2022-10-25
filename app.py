43a0b3d45540fb69a18534f8a171319838620f96
folaulau <folaukaveinga@gmail.com>
2022-10-24 19:01:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World1!'
+    return 'Hello World!'
 
 @app.route('/ping')
 def ping():  # put application's code here
